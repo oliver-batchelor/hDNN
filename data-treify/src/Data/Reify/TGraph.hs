5e4c26cab0cec1e67f1a2945a99eca157e47d5d8
Oliver Batchelor <saulzar@gmail.com>
2016-09-29 15:13:28 +1300

    Better type representation

--- data-treify/src/Data/Reify/TGraph.hs
+++ data-treify/src/Data/Reify/TGraph.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE GADTs, ExistentialQuantification, FlexibleContexts,
-    UndecidableInstances, PatternGuards, KindSignatures,
-    ConstraintKinds, TypeOperators, MultiParamTypeClasses,
-    TypeFamilies, FlexibleInstances, ScopedTypeVariables, Rank2Types
-#-}
+   UndecidableInstances, PatternGuards, ConstraintKinds,
+   TypeOperators, MultiParamTypeClasses, TypeFamilies,
+   FlexibleInstances, ScopedTypeVariables, Rank2Types #-}
 
 module Data.Reify.TGraph
   ( ShowF(..), Id, V(..), Bind(..), bindEnv, Graph(..), Yes, Yes2, IsTy(..), IsTy2(..)
@@ -10,31 +9,9 @@ module Data.Reify.TGraph
 
 -- For bindEnv
 import Data.Type.Equality
-import GHC.Exts (Constraint)
 import qualified Data.IntMap as I
 
-
-class Yes (f :: * -> *) a
-instance Yes f a
-
--- | Type class for typed type representations
-class IsTy f where
-  type IsTyConstraint f z :: Constraint
-  type IsTyConstraint f z = Yes f z
-  tyEq :: (IsTyConstraint f a, IsTyConstraint f b) =>
-          f a -> f b -> Maybe (a :~: b)
-
-class Yes2 (f :: * -> * -> *) a b
-instance Yes2 f a b
-
--- | Type class for typed type representations
-class IsTy2 f where
-  type IsTy2Constraint f u v :: Constraint
-  type IsTy2Constraint f u v = Yes2 f u v
-  tyEq2 :: (IsTy2Constraint f a b, IsTy2Constraint f c d) =>
-           f a b -> f c d -> Maybe ((a,b) :~: (c,d))
-
-
+import Data.IsTy
 
 -- | Identifiers
 type Id = Int
