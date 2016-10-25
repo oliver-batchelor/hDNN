af817fc0399eafa868c899ecc6bae727dbfa5cfd
Oliver Batchelor <saulzar@gmail.com>
2016-10-25 18:16:05 +1300

    Graph
    a

---
+++ src/HDNN/TGraph.hs
@@ -0,0 +1,71 @@
+
+module HDNN.TGraph
+  ( ShowF(..), V(..), Bind(..), bindEnv, Graph(..),
+  ) where
+
+-- For bindEnv
+import Data.Type.Equality
+import qualified Data.IntMap as I
+
+import Prelude
+
+-- | Typed variables
+data V ty a = V Int (ty a)
+
+instance Eq (V ty a) where V i _ == V j _ = i == j
+
+class ShowF f where
+  showF :: f a -> String                -- maybe Show a =>
+
+instance ShowF (V ty) where
+  -- showF (V i t) = parens $ show i ++ "::" ++ show t
+  showF (V i _) = 'x' : show i
+
+-- parens :: String -> String
+-- parens = ("(" ++) . (++ ")")
+
+instance Show (V ty a) where show = showF
+
+-- | Typed binding pair, parameterized by variable and node type
+-- constructors.
+data Bind ty n where
+  Bind :: V ty a -> n (V ty) a -> Bind ty n
+
+instance ShowF (n (V ty)) => Show (Bind ty n) where
+  show (Bind v n) = showF v ++" = "++ showF n
+
+{-
+-- Slow version
+bindEnv' :: IsTy ty => [Bind ty n] -> (V ty a -> n (V ty) a)
+bindEnv' [] _ = error "bindEnv': ran out of bindings"
+bindEnv' (Bind (V i a) n : binds') v@(V i' a')
+  | Just Refl <- a `tyEq` a', i == i' = n
+  | otherwise                         = bindEnv' binds' v
+-}
+
+-- | Fast version, using an IntMap.
+-- Important: partially apply.
+bindEnv :: forall ty n. TestEquality ty => [Bind ty n] -> (forall a. V ty a -> n (V ty) a)
+bindEnv binds = \ (V i' a') -> extract a' (I.lookup i' m)
+ where
+   m :: I.IntMap (Bind ty n)
+   m = I.fromList [(i,b) | b@(Bind (V i _) _) <- binds]
+--    extract :: forall a'. IsTyConstraint ty a' =>
+--               ty a' -> Maybe (Bind ty n) -> n (V ty) a'
+   extract ::  ty a -> Maybe (Bind ty n) -> n (V ty) a
+   extract _ Nothing            = error "bindEnv: variable not found"
+   extract a' (Just (Bind (V _ a) n))
+     | Just Refl <- a `testEquality` a' = n
+     | otherwise                = error "bindEnv: wrong type"
+
+-- TODO: Does the partial application *really* avoid the IntMap reconstruction?
+
+-- | Graph, described by bindings and a root variable
+data Graph ty n a = Graph [Bind ty n] (V ty a)
+
+
+-- We could generalize V out of Bind and Graph.
+
+
+instance (ShowF (n (V ty)), Show (V ty a)) => Show (Graph ty n a) where
+  show (Graph netlist start) = "let " ++ show netlist ++ " in " ++ show start
